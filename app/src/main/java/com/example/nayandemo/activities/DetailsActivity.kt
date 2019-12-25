3fda774ff7e0ca25ce77321bd4fd1b110313a43d
kaulsalil88 <salil.kaul@tothenew.com>
2019-12-25 20:19:39 +0530

    Passing data to the details screen and displaying the same .

--- app/src/main/java/com/example/nayandemo/activities/DetailsActivity.kt
+++ app/src/main/java/com/example/nayandemo/activities/DetailsActivity.kt
@@ -2,12 +2,17 @@ package com.example.nayandemo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.nayandemo.R
+import com.example.nayandemo.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
+    lateinit var detailsBinding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        detailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_details)
+        detailsBinding.repo = intent.getParcelableExtra("repoData")
+
     }
 }
