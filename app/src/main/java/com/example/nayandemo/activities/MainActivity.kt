43b34dfa2d28fa923bed825c3e4949702e544f58
kaulsalil88 <salil.kaul@tothenew.com>
2019-12-25 19:30:22 +0530

    Basic set up of recycler view done .

--- app/src/main/java/com/example/nayandemo/activities/MainActivity.kt
+++ app/src/main/java/com/example/nayandemo/activities/MainActivity.kt
@@ -2,18 +2,32 @@ package com.example.nayandemo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nayandemo.R
+import com.example.nayandemo.databinding.ActivityMainBinding
 import viewmodels.MainViewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
         viewModel.getRepositories()
+        viewModel.result.observe(this, Observer {
+            if (it != null && it.size > 0) {
+                // val layoutManager = RecyclerView.LayoutManager(RecyclerView.LayoutManager.)
+                val repoAdapter = RepositoryAdapter(it)
+                mainBinding.rvRepos.adapter = repoAdapter
+
+            }
+        })
     }
 }
